609ae2ea3c2c7760166dc2b5405b3b6c3f2babf0
DisgustingGust <0apm.tv@gmail.com>
2017-01-17 19:44:31 +0300

    done v2

---
+++ eXist-db_exide/eXist-db XQJ/src/QuickStart.java
@@ -0,0 +1,25 @@
+import javax.xml.xquery.*;
+import javax.xml.namespace.QName;
+import net.xqj.exist.ExistXQDataSource;
+
+public class QuickStart {
+    public static void main(String[] args) throws XQException {
+        XQDataSource xqs = new ExistXQDataSource();
+        xqs.setProperty("serverName", "localhost");
+        xqs.setProperty("port", "8080");
+
+        XQConnection conn = xqs.getConnection();
+
+        XQPreparedExpression xqpe =
+                conn.prepareExpression("declare variable $x as xs:string external; $x");
+
+        xqpe.bindString(new QName("x"), "Hello World!", null);
+
+        XQResultSequence rs = xqpe.executeQuery();
+
+        while (rs.next())
+            System.out.println(rs.getItemAsString(null));
+
+        conn.close();
+    }
+}
